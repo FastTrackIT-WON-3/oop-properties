1b44b2a212c59809e15abb812f3a097b68f90ca0
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-02-07 19:22:45 +0200

    a simple example with properties

---
+++ OopProperties/OopProperties/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopProperties
+{
+    public class Person
+    {
+        private string firstName = string.Empty;
+        private string lastName = string.Empty;
+        private string cnp;
+        private DateTime dateOfBirth;
+
+        public Person(string cnp, DateTime dateOfBirth)
+        {
+            this.cnp = cnp;
+            this.dateOfBirth = dateOfBirth;
+        }
+
+        public string FirstName 
+        {
+            get { return firstName; }
+            set { firstName = value ?? string.Empty; }
+        }
+
+        public string LastName 
+        {
+            get { return lastName; }
+            set { lastName = value ?? string.Empty; }
+        }
+
+        public string CNP 
+        {
+            get { return cnp; }
+        }
+
+        public DateTime DateOfBirth 
+        {
+            get { return dateOfBirth; }
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello, my name is {FirstName} {LastName}");
+            Console.WriteLine($"My CNP is {CNP}");
+            Console.WriteLine($"My date of birth is {DateOfBirth}");
+        }
+    }
+}
---
+++ OopProperties/OopProperties/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OopProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("1234434343", new DateTime(1980, 2, 7));
+            p.FirstName = "John";
+            p.LastName = "Doe";
+            p.SayHello();
+            // Console.WriteLine($"{p.CNP}");
+        }
+    }
+}
